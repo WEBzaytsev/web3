d7afa196d462a4aa2ea4c5e607c0b8fbe9efc277
WEBZaytsev <75935616+WEBzaytsev@users.noreply.github.com>
2022-09-06 16:02:33 +0300

    snax

---
+++ wp-content/themes/newsblock-child/snax/posts/form-edit/row-legal.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Snax News Post Row Legal
+ *
+ * @package snax 1.11
+ * @subpackage Theme
+ */
+
+?>
+
+<?php if ( snax_legal_agreement_required() ) : ?>
+	<div class="snax-edit-post-row-legal<?php echo snax_has_field_errors( 'legal' ) ? ' snax-validation-error' : ''; ?>">
+
+		<?php if ( snax_has_field_errors( 'legal' ) ) : ?>
+			<span class="snax-validation-tip"><?php echo esc_html( snax_get_field_errors( 'legal' ) ); ?></span>
+		<?php endif; ?>
+
+		<label>
+			<input type="checkbox" id="snax-post-legal" name="snax-post-legal" required <?php checked( snax_get_field_values( 'legal' ) ); ?> /> <?php esc_html_e( 'I agree with the terms and conditions.', 'snax' ); ?>
+			<?php snax_render_legal_page_link(); ?>
+    </label>
+
+
+	</div>
+<?php endif; ?>
