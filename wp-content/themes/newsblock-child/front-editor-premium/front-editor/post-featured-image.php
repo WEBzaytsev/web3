a0989659a88a5da4416152d20f4d217669e0b8cd
infiniteqx0 <infiniteqx0@gmail.com>
2022-09-27 13:14:25 +0300

    Add image upload error element

--- wp-content/themes/newsblock-child/front-editor-premium/front-editor/post-featured-image.php
+++ wp-content/themes/newsblock-child/front-editor-premium/front-editor/post-featured-image.php
@@ -22,3 +22,4 @@ if (has_post_thumbnail($post_id)) {
     <img <?= $style ?? '' ?> id="post_thumbnail_image" src="data:image/jpeg;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=" />
     <img src="<?= FE_PLUGIN_URL . '/assets/img/cancel.svg' ?>" class="bfe-remove-image">
 </div>
+<div class="image_loader__error"></div>
